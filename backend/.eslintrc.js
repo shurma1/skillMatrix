f4de5c8cae613619fdbed4a215c0fcaa0140ba6d
shurma1 <dmitriy.prokhorov.04@mail.ru>
2025-07-23 17:19:46 +0300

    add eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'indent': ['error', 4],
+        '@typescript-eslint/indent': ['error', 4],
+    },
+};
